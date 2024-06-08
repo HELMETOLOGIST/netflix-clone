28e998e77766490f11b59f912e92fb01b9560f84
Nijith <nijithcv@gmail.com>
2024-06-08 11:21:53 +0530

    publish

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'build', // ensure this is 'build' or change your deploy script accordingly
+  },
+  base: '/netflix-clone/' // replace 'to-do-list' with your actual repository name
+});
